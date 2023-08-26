36a5ce812b9f26447a0fc3412d27115122327774
sora <sorazora1.4@gmail.com>
2023-08-25 21:28:08 -0700

    displays the modal on the screen

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-'use strict';
+"use strict";
+
+// DOM Elements
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+
+// Adding event listeners to all buttons
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener("click", () => {
+    // Removing the 'hidden' class to display the modal and overlay
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
